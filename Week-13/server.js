4db04341fedf94282a62c3f628c16b54500d70f5
Sameer Ajmani <47604025+ajmanisameer@users.noreply.github.com>
2022-11-06 15:06:20 +0530

    Routes setup

---
+++ Week-13/server.js
@@ -0,0 +1,11 @@
+
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+// app.use('/', (req, res) => res.send("Hello world"));
+app.use('/api', require('./routes/routes'));
+
+
+app.listen(port, () => console.log('BackEnd Server Is On=', port));
